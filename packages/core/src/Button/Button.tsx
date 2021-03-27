b2b9613ce1657715e61ca2ced59e8404cc45cc60
Daniele Irsuti <irsooti@gmail.com>
2021-03-27 15:01:35 +0100

    added addon-controls

--- packages/core/src/Button/Button.tsx
+++ packages/core/src/Button/Button.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useState } from 'react';
 import style from './style.module.css';
 
-const Button: FC = () => {
+const Button: FC<{ value: string }> = ({ value }) => {
   const [state, setState] = useState();
-  return <input type="button" className={style.prova} value="ciaone" />;
+  return <input type="button" className={style.prova} value={value} />;
 };
 
 export default Button;
--- packages/stories/src/Button/Button.stories.tsx
+++ packages/stories/src/Button/Button.stories.tsx
@@ -1,12 +1,19 @@
-/* eslint-disable import/prefer-default-export */
 import React from 'react';
 import { Button } from '@design/core';
 
-export const Primary = () => <Button />;
+export const Primary = ({ label }) => <Button value={label} />;
+
 Primary.parameters = {
   jest: ['../../tests/src/Button/Button.test.js'],
 };
+
 export default {
   title: 'YourComponent',
   component: Button,
+  args: {
+    label: 'hello',
+  },
+  argTypes: {
+    label: { control: { type: 'text' } },
+  },
 };
