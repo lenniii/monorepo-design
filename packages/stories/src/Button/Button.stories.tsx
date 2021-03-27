e54195cd38777215f08bf4cef31a1fc205517c73
Dennis Alessandrini <dennis.alessandrini@aesys.tech>
2021-03-28 00:32:24 +0100

    fix: ci test

--- packages/stories/src/Button/Button.stories.tsx
+++ packages/stories/src/Button/Button.stories.tsx
@@ -11,7 +11,7 @@ export default {
   title: 'YourComponent',
   component: Button,
   args: {
-    label: 'test123test',
+    label: 'prova123prova',
   },
   argTypes: {
     label: { control: { type: 'text' } },
