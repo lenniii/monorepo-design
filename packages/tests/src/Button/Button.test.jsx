3c3bef6319373eb6c95a19aaa365ee07862e2812
Dennis Alessandrini <dennis.alessandrini@aesys.tech>
2021-03-27 23:19:38 +0100

    chore: add husky

--- packages/tests/src/Button/Button.test.jsx
+++ packages/tests/src/Button/Button.test.jsx
@@ -5,10 +5,5 @@ import { Button } from '@design/core';
 
 test('this should be working and pass', () => {
   render(<Button value="ciaone" />);
-  expect(screen.queryByDisplayValue('ciaone')).toHaveValue('ciaone');
-});
-
-test('this should be working and fail', () => {
-  render(<Button value="asd" />);
-  expect(false).toBeTruthy();
+  expect(screen.queryByDisplayValue('ciaone')).toBeInTheDocument();
 });
