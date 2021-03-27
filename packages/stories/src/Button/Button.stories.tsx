3d996a858031322419f77e42db9fc5f014c03c88
Dennis Alessandrini <dennis.alessandrini@aesys.tech>
2021-03-27 14:37:38 +0100

    inutile

--- packages/stories/src/Button/Button.stories.tsx
+++ packages/stories/src/Button/Button.stories.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import { Button } from '@design/core';
 
 export const Primary = () => <Button />;
-Primary.parameters = {
-  jest: ['../../tests/src/Button/Button.test.js'],
-};
+
 export default {
   title: 'YourComponent',
   component: Button,
