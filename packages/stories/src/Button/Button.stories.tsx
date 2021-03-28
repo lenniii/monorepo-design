df2c5cccb6e3e777380b1431f499517cdd06e08c
lenniii <dennisalessandrini@gmail.com>
2021-03-28 18:50:59 +0200

    chore: fix eslint stories

--- packages/stories/src/Button/Button.stories.tsx
+++ packages/stories/src/Button/Button.stories.tsx
@@ -3,10 +3,6 @@ import { Button } from '@design/core';
 
 export const Primary = ({ label }) => <Button value={label} />;
 
-Primary.parameters = {
-  jest: ['../../tests/src/Button/Button.test.js'],
-};
-
 export default {
   title: 'YourComponent',
   component: Button,
