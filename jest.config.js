6f76a51184efbdffe5ce0fb79820ad5a1a5ca60a
lenniii <dennisalessandrini@gmail.com>
2021-03-28 19:39:11 +0200

    ci: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 module.exports = {
-  transform: { '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }] },
+  transform: {
+    '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css',
+  },
   coverageDirectory: './packages/tests/coverage',
   collectCoverageFrom: ['packages/core/dist/*/*.js', '!packages/core/dist/_virtual/*.js'],
 };
